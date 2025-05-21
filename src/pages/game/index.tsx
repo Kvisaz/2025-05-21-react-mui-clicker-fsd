eb6090c820189bf18adde669a1aab82d76d4b7a8
sergeytokarev <stokarev@novakidschool.com>
2025-05-21 23:30:18 +0700

    feat: add automatic resource updates every second

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -1,10 +1,23 @@
 import { Container, Box } from '@mui/material';
+import { useEffect } from 'react';
 import { GameHeader } from '../../widgets/gameHeader';
 import { GameFooter } from '../../widgets/gameFooter';
 import { UpgradeList } from '../../widgets/upgradeList';
 import { ClickArea } from '../../features/clicking';
+import { useGameStore } from '../../entities/player/model/store';
 
 export const GamePage = () => {
+  const { updateOfflineProgress } = useGameStore();
+
+  // Update resources every second
+  useEffect(() => {
+    const interval = setInterval(() => {
+      updateOfflineProgress();
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [updateOfflineProgress]);
+
   return (
     <Container maxWidth="lg">
       <Box sx={{ 
